6b1c43613fdac319834a3e90f7e183c3d9388867
Ricardo P. <perez.ricardo.p1@gmail.com>
2025-02-01 05:45:28 +0000

    first commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     public static void main(String[] args){
         //output hello world
 
-        System.out.println("Hello world"); 
+        System.out.println("Hello world!"); 
     }
 
 }
